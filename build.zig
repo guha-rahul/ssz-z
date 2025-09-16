c0c21d13b1f43e1d24155c4f8f1857cfbefae066
Rahul Guha <19rahul2003@gmail.com>
2025-09-17 03:13:32 +0530

    upgrade spec test version

--- build.zig
+++ build.zig
@@ -24,7 +24,7 @@ pub fn build(b: *std.Build) void {
     const options_spec_test_options = b.addOptions();
     const option_spec_test_url = b.option([]const u8, "spec_test_url", "") orelse "https://github.com/ethereum/consensus-spec-tests";
     options_spec_test_options.addOption([]const u8, "spec_test_url", option_spec_test_url);
-    const option_spec_test_version = b.option([]const u8, "spec_test_version", "") orelse "v1.5.0";
+    const option_spec_test_version = b.option([]const u8, "spec_test_version", "") orelse "v1.6.0-alpha.4";
     options_spec_test_options.addOption([]const u8, "spec_test_version", option_spec_test_version);
     const option_spec_test_out_dir = b.option([]const u8, "spec_test_out_dir", "") orelse "test/spec/spec_tests";
     options_spec_test_options.addOption([]const u8, "spec_test_out_dir", option_spec_test_out_dir);
